3934fc4136d63a672de387eb5ce1a8b39456f3f8
Jernej Zorec <jernejz@gmail.com>
2019-04-10 23:22:10 +0200

    Initial commit

---
+++ Fastfile.swift
@@ -0,0 +1,239 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+    
+    var appleID: String?
+    var defaultAppleID: String = fallbackAppleId
+    
+    func beforeAll() {
+        appleID = prompt(text: "Apple ID: ")
+    }
+    
+    func afterAll(currentLane: String) {}
+    
+    func onError(currentLane: String, errorInfo: String) {
+        slackError(message: errorInfo)
+    }
+    
+	func betaLane() {
+	desc("Push a new beta build to TestFlight")
+		syncCodeSigning(
+            type: "appstore",
+            readonly: true,
+            appIdentifier: [appIdentifier],
+            username: appleID ?? defaultAppleID,
+            teamId: teamID,
+            gitUrl: matchGitUrl,
+            gitBranch: matchGitBranch,
+            shallowClone: true,
+            cloneBranchDirectly: true
+        )
+        automaticCodeSigning(
+            path: projectPath,
+            useAutomaticSigning: false
+        )
+		buildBumpLane()
+        cocoapods()
+		buildApp(workspace: projectWorkspace, scheme: projectScheme)
+        automaticCodeSigning(
+            path: projectPath,
+            useAutomaticSigning: true
+        )
+		uploadToTestflight(
+            username: appleID ?? defaultAppleID,
+            skipSubmission: true,
+            skipWaitingForBuildProcessing: true,
+            teamId: itcTeam
+        )
+	}
+    
+    // MARK: Codes signing
+    
+    public func certificatesForReleaseLane() {
+        match(
+            type: "appstore",
+            readonly: false,
+            appIdentifier: [appIdentifier],
+            username: appleID ?? defaultAppleID,
+            teamId: teamID,
+            teamName: teamID,
+            gitUrl: matchGitUrl,
+            gitBranch: matchGitBranch
+        )
+    }
+    
+    public func refreshProfilesLane() {
+        refreshAppstoreProfiles()
+    }
+    
+    // MARK: Private lanes
+    
+    private func refreshAppstoreProfiles() {
+        match(
+            type: "appstore",
+            readonly: false,
+            appIdentifier: [appIdentifier],
+            teamId: teamID,
+            gitUrl: matchGitUrl,
+            gitBranch: matchGitBranch
+        )
+    }
+    
+    // MARK: Helpers
+    
+    public func testLocalLane() {
+        slackSuccess(message: "woho")
+    }
+    
+    public func testSlackLane() {
+        slackNotify(message: "Lorem ipsum NOTIFY :arenaprod:")
+        slackSuccess(message: "Lorem ipsum SUCCESS :arenastage:")
+        slackError(message: "Lorem ipsum ERROR :arenadev:")
+    }
+    
+    private func versionBumpLane(versionNumber: String? = nil) {
+        let newVersionNumber = incrementVersionNumber(versionNumber: versionNumber).trim()
+        let message = "Version bump \(newVersionNumber) by fastlane"
+        
+        commitVersionBump(
+            message: message,
+            xcodeproj: projectPath,
+            force: true
+        )
+        
+        pushToGitRemote(force: false)
+        
+        let slackMessage = "Version bump \(newVersionNumber)"
+        slackNotify(message: slackMessage)
+    }
+    
+    private func buildBumpLane(buildNumber: String? = nil, commitPrefix: String = "Build bump", force: Bool = false) {
+        let lastCommit = lastGitCommit()
+        
+        if force == false {
+            guard lastCommit["message"]?.hasPrefix("Build bump") == false && buildNumber == nil else {
+                println(message: "No need for a build bump")
+                return
+            }
+        }
+        
+        let newBuildNumber = incrementBuildNumber(buildNumber: buildNumber).trim()
+        let message = "\(commitPrefix) \(newBuildNumber) by fastlane"
+        
+        commitVersionBump(
+            message: message,
+            xcodeproj: projectPath,
+            force: true
+        )
+        
+        pushToGitRemote(force: false)
+        
+        let versionNumber = getVersionNumber(target: projectScheme).trim()
+        let slackMessage = "\(commitPrefix) version \(newBuildNumber) build \(versionNumber)"
+        slackNotify(message: slackMessage)
+    }
+    
+    public func checkTargetsLane() {
+        let checkTargetsOutput = sh(command: "./fastlane/check_targets.sh")
+        if checkTargetsOutput.contains("has a different number of files!") {
+            slackError(message: checkTargetsOutput)
+            exit(1)
+        }
+    }
+    
+    func downloadAssetsLane() {
+//        let someFile = ""
+//        if FileManager.default.fileExists(atPath: libavcodecFile) {
+//            println(message: "{somefile} already exists. No need to download.")
+//        } else {
+//            sh(command: "wget -q \"https://www.dropbox.com/s/XXX?dl=1\" -O \(libavcodecFile)")
+//        }
+    }
+    
+    func changelogSinceLastBuildBump() -> String {
+        let lastCommit = sh(command: "git log --pretty=format:'%H' -n 1")
+        let lastBuildBumpCommit = sh(command: "git log --pretty=format:'%H' --grep='Build bump' --skip 1 -n 1 ")
+        
+        let changelog = changelogFromGitCommits(
+            between: "\(lastCommit),\(lastBuildBumpCommit)",
+            pretty: "%s <%an>",
+            dateFormat: "short",
+            mergeCommitFiltering: "exclude_merges"
+        )
+        
+        return changelog
+    }
+    
+    func changelogSinceLastTestflight() -> String {
+        let changelog = changelogFromGitCommits(
+            pretty: "%s <%an>",
+            dateFormat: "short",
+            tagMatchPattern: "testflight/*",
+            mergeCommitFiltering: "exclude_merges"
+        )
+        
+        return changelog
+    }
+    
+    func tagTestflightBuildLane() {
+        let lastCommit = lastGitCommit()
+        let buildNumber = getBuildNumber().trim()
+        addGitTag(tag: "testflight/\(buildNumber)", buildNumber: buildNumber, commit: lastCommit["commit_hash"])
+        pushGitTags()
+    }
+    
+    func tagStageBuildLane() {
+        let lastCommit = lastGitCommit()
+        let buildNumber = getBuildNumber().trim()
+        addGitTag(tag: "stage/\(buildNumber)", buildNumber: buildNumber, commit: lastCommit["commit_hash"])
+        pushGitTags()
+    }
+    
+    func tagAppstoreVersionLane() {
+        let lastCommit = lastGitCommit()
+        let versionNumber = getVersionNumber().trim()
+        let buildNumber = getBuildNumber().trim()
+        addGitTag(tag: "appstore/\(versionNumber)", buildNumber: buildNumber, commit: lastCommit["commit_hash"])
+        pushGitTags()
+    }
+}
+
+extension Fastfile {
+    private func slackNotify(message: String, pretext: String = "") {
+        slack(message: message,
+              channel: slackChannel,
+              slackUrl: slackUrl,
+              defaultPayloads: [],
+              attachmentProperties: [
+                "color": "warn",
+                "pretext": pretext,
+                "text": message
+            ]
+        )
+    }
+    private func slackSuccess(message: String) {
+        slack(message: message, channel: slackChannel, slackUrl: slackUrl, defaultPayloads: [], success: true)
+    }
+    private func slackError(message: String) {
+        slack(message: message, channel: slackChannel, slackUrl: slackUrl, defaultPayloads: [], success: false)
+    }
+}
+
+extension String {
+    func trim() -> String {
+        var string = self.replacingOccurrences(of: "\\n", with: "", options: .regularExpression)
+        string = string.replacingOccurrences(of: "\"", with: "", options: .regularExpression)
+        return string
+    }
+    var removingWhitespacesAndNewlines: String {
+        return replacingOccurrences(of: "\"", with: "", options: .regularExpression)
+    }
+}
